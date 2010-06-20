f059f44c14e2c9fd7eb1feae6badf15cb218ff2b
Michael Rodriguez <dkingston02@gmail.com>
2010-06-20 20:48:53 +0800

    more channel work. its almost here.

--- libnuclix/channel.py
+++ libnuclix/channel.py
@@ -11,6 +11,7 @@ from time import time
 import var
 import logger
 import protocol
+import event
 
 def add(name, ts):
     '''Add the channel to the hash list.'''
@@ -42,6 +43,45 @@ def delete(name):
     var.channels[name]['topic'] = None
     var.channels[name]['topic_setter'] = None
     var.channels[name]['metadata'] = []
+
+def delete_user(channel, user):
+    '''Remove a user from a channel.'''
+
+    try:
+        var.channels[channel]
+    except KeyError:
+        logger.debug('attempted to remove user %s from channel %s, but that channel does not exist' % (user['nick'], channel))
+        return
+
+    event.dispatch('OnUserPart', channel, user)
+
+    var.channels[name]['ucount'] -= 1
+
+    if var.channels[name]['ucount'] == 0:
+        logger.debug('channel %s became empty after %s parted, removing' % (channel['name'], user['nick']))
+        delete_channel(channel)
+
+    logger,debug('user %s from %s removed' % (user['nick'], channel['name']))
+
+def delete_channel(channel):
+    '''Remove a channel, it's users, everything.'''
+
+    try:
+        var.channels[channel]
+    except KeyError:
+        logger.debug('attempted to delete non-existant channel %s' % channel)
+        return
+
+    event.dispatch('OnDeleteChannel', channel)
+
+    var.channels[channel]['ts'] = 0
+    var.channels[channel]['ucount'] = 0
+    var.channels[channel]['users'] = []
+    var.channels[channel]['topic'] = None
+    var.channels[channel]['topic_setter'] = None
+    var.channels[channel]['metadata'] = []
+
+    logger.debug('deleted channel %s' % channel['name'])
 
 def maintain():
     '''Maintain the channels, sorting them out, etc.'''
